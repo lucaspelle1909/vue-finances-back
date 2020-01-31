5e4d3899a0e9c694c361a36f09c82aed90115f17
lucaspelle1909 <lucas.pelle@hotmail.com>
2020-01-31 17:56:32 -0300

    Criando resolvers e outros

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const { GraphQLServer } = require("graphql-yoga");
+const Binding = require("prisma-binding");
+const { prisma } = require("./generated/prisma-client");
+
+const resolvers = require("./resolvers");
+
+const server = new GraphQLServer({
+  typeDefs: `${__dirname}/schema.graphql`,
+  resolvers,
+  context: (request) => ({
+    db: new Binding.Prisma({
+      typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
+      endpoint: process.env.PRISMA_ENDPOINT
+    }),
+    prisma,
+    ...request,
+  })
+});
+
+server.start().then(() => console.log("Server running..."));
---
+++ src/resolvers/Query.js
@@ -0,0 +1,13 @@
+const { getUserId } = require('./../utils')
+
+
+function user(_, args, ctx, info) {
+  
+  const userId = getUserId(ctx);
+
+  return ctx.db.query.user({ where: { id: userId } }, info);
+}
+
+module.exports = {
+  user
+}
