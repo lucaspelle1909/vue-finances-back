c55055c1253d78213058c01824a4ff00c7c822fa
lucaspelle1909 <lucas.pelle@hotmail.com>
2020-02-04 16:10:21 -0300

    Conclusão API GraphQL

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ const { prisma } = require("./generated/prisma-client");
 
 const resolvers = require("./resolvers");
 
+const env = process.env;
+
+const endpoint = `${env.PRISMA_ENDPOINT}/${env.PRISMA_SERVICE}/${env.PRISMA_STAGE}`;
+
 const server = new GraphQLServer({
   typeDefs: `${__dirname}/schema.graphql`,
   resolvers,
   context: (request) => ({
     db: new Binding.Prisma({
       typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
-      endpoint: process.env.PRISMA_ENDPOINT
+      endpoint
     }),
     prisma,
     ...request,
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,58 +1,119 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const moment = require("moment");
 const { getUserId } = require("./../utils");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-async function createAccount(_, { description }, ctx, info){
+async function createAccount(_, { description }, ctx, info) {
   const userId = getUserId(ctx);
 
-  return ctx.db.mutation.createAccount({
-    data: {
-      description,
-      user: {
-        connect: {
-          id: userId
+  return ctx.db.mutation.createAccount(
+    {
+      data: {
+        description,
+        user: {
+          connect: {
+            id: userId
+          }
         }
       }
-    }
-  }, info);
+    },
+    info
+  );
+}
+
+async function createCategory(_, { description, operation }, ctx, info) {
+  const userId = getUserId(ctx);
+
+  return ctx.db.mutation.createCategory(
+    {
+      data: {
+        description,
+        operation,
+        user: {
+          connect: {
+            id: userId
+          }
+        }
+      }
+    },
+    info
+  );
+}
+
+async function createRecord(_, args, ctx, info) {
+
+  const date = moment(args.date); //2019-08-11
+
+  if(!date.isValid()){
+    throw new Error("Invalid date");
+  }
+
+  const userId = getUserId(ctx);
+
+  return ctx.db.mutation.createRecord(
+    {
+      data: {
+        user: {
+          connect: { id: userId }
+        },
+        account: {
+          connect: { id: args.accountId }
+        },
+        category: {
+          connect: { id: args.categoryId }
+        },
+        amount: args.amount,
+        type: args.type,
+        date: args.date,
+        description: args.description,
+        tags: args.tags,
+        note: args.node
+      }
+    },
+    info
+  );
 }
 
 async function login(_, { email, password }, ctx, info) {
   const user = await ctx.db.query.user({ where: { email } });
-  if(!user){
-    throw new Error('Invalid credentials!');
+  if (!user) {
+    throw new Error("Invalid credentials!");
   }
 
   const valid = await bcrypt.compare(password, user.password);
-  if(!valid){
-    throw new Error('Invalid credentials');
+  if (!valid) {
+    throw new Error("Invalid credentials");
   }
 
-  const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '2h' });
+  const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "2h" });
 
   return {
     token,
     user
-  }
+  };
 }
 
 async function signup(_, args, ctx, info) {
-
   const password = await bcrypt.hash(args.password, 10);
-  const user = await ctx.db.mutation.createUser({ data: { ...args, password } });
 
-  const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '2h' });
+  const user = await ctx.db.mutation.createUser({
+    data: { ...args, password }
+  });
+
+  const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "2h" });
 
   return {
     token,
     user
-  }
+  };
 }
 
 module.exports = {
   login,
   signup,
-  createAccount
-} 
+  createAccount,
+  createCategory,
+  createRecord
+};
